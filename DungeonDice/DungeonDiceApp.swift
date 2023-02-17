47c323695e39743c2f5602f38a4a9254dc0b99e3
Christopher Kennedy <kennednu@bc.edu>
2023-02-17 14:36:19 -0500

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Christopher Kennedy on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
